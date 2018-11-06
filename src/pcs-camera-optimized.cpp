5247c30c5080e51cce1980d3503525f3da409f0f
Artur Balanuta <3759560+ABalanuta@users.noreply.github.com>
2018-11-06 05:04:31 -0500

    SIMD and OpenMP version

--- src/pcs-camera-optimized.cpp
+++ src/pcs-camera-optimized.cpp
@@ -13,18 +13,70 @@
 #include <getopt.h>
 #include <signal.h>
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #include <librealsense2/rs.hpp>
 
+#include <omp.h>
+#include <immintrin.h>
+#include <xmmintrin.h>
+
 #define TIME_NOW    std::chrono::high_resolution_clock::now()
-#define BUF_SIZE    4000000
-#define CONV_RATE   1000
+#define BUF_SIZE    5000000
+#define CONV_RATE   1000.0
+#define DOWNSAMPLE  1
+#define PORT        8000
+
+
 
 typedef std::chrono::high_resolution_clock clockTime;
 typedef std::chrono::duration<double, std::milli> timeMilli;
 typedef std::chrono::time_point<clockTime> timestamp;
 
 char *filename = "samples.bag";
+
+bool display_updates = false;
+bool send_buffer = false;
+bool cutoff = false;
+
+int num_of_threads = 1;
+int client_sock = 0;
+int sockfd = 0;
 timestamp time_start, time_end;
+
+// Creates TCP stream socket and connects to the central computer.
+void initSocket(int port) {
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        std::cerr << "\nSocket fd not received." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "\nBind failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sockfd, 3) < 0) {
+        std::cerr << "\nListen failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Waiting for client..." << std::endl;
+
+    if ((client_sock = accept(sockfd, NULL, NULL)) < 0) {
+        std::cerr << "\nConnection failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Established connection with client_sock: " << client_sock << std::endl;
+}
 
 void sendXYZRGBPointcloud(rs2::points pts, rs2::video_frame color, short * buffer);
 
@@ -35,13 +87,13 @@ void sigintHandler(int dummy) {
 }
 
 void print_usage() {
-    printf("\nUsage: pcs-camera-test-samples -f <samples.bag>\n\n");
+    printf("\nUsage: pcs-camera-test-samples -f <samples.bag> -v (visualize)\n\n");
 }
 
 // Parse arguments for extra runtime options
 void parseArgs(int argc, char** argv) {
     int c;
-    while ((c = getopt(argc, argv, "hf:")) != -1) {
+    while ((c = getopt(argc, argv, "hf:vst:c:")) != -1) {
         switch(c) {
             case 'h':
                 print_usage();
@@ -49,13 +101,23 @@ void parseArgs(int argc, char** argv) {
             case 'f':
                 filename = optarg;
                 break;
+            case 'v':
+                display_updates = true;
+                break;
+            case 's':
+                send_buffer = true;
+                break;
+            case 't':
+                num_of_threads = atoi(optarg);
+                break;
+            case 'c':
+                cutoff = true;
+                break;
         }
     }
 
     std::cout << "\Reading Frames from File: " << filename << std::endl;
 }
-
-
 
 int main (int argc, char** argv) {
 
@@ -74,10 +136,11 @@ int main (int argc, char** argv) {
 
     rs2::device device = pipe.get_active_profile().get_device();
     std::cout << "Camera Info: " << device.get_info(RS2_CAMERA_INFO_NAME) << " FW ver:" << device.get_info(RS2_CAMERA_INFO_FIRMWARE_VERSION) << std::endl;
+    if (num_of_threads) std::cout << "OpenMP Threads: " << num_of_threads << std::endl;
     
-    auto depth_stream = selection.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
-    auto resolution = std::make_pair(depth_stream.width(), depth_stream.height());
-    auto intr = depth_stream.get_intrinsics();
+    //auto depth_stream = selection.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
+    //auto resolution = std::make_pair(depth_stream.width(), depth_stream.height());
+    //auto intr = depth_stream.get_intrinsics();
 
 
     //get_extrinsics(const rs2::stream_profile& from_stream, const rs2::stream_profile& to_stream)
@@ -87,7 +150,8 @@ int main (int argc, char** argv) {
     short *buffer = (short *)malloc(sizeof(short) * BUF_SIZE);
     
     rs2::frameset frames;
-    
+
+    if (send_buffer) initSocket(PORT);
     
     while (true)
     {    
@@ -97,8 +161,9 @@ int main (int argc, char** argv) {
             continue;
         }
         else
-        {   
-            if (last_frame > frames.get_frame_number()) break;
+        {
+
+            if (!send_buffer && last_frame > frames.get_frame_number()) break;
             if (last_frame == frames.get_frame_number()) continue;
             
             //std::cout << "Got Frame # " << last_frame << std::endl;
@@ -106,77 +171,209 @@ int main (int argc, char** argv) {
             i++;
 
             //use frames here
-            time_start = TIME_NOW;
+            
                                                                 // stairs.bag vs sample.bag
             rs2::video_frame color = frames.get_color_frame();  // 0.003 ms vs 0.001ms
             rs2::depth_frame depth = frames.get_depth_frame();  // 0.001ms vs 0.001ms
-            rs2::points pts = pc.calculate(depth);              // 27ms vs 27ms
-                        
+            rs2::points pts = pc.calculate(depth);              // 27ms vs 27ms            
             pc.map_to(color);       // 0.01ms vs 0.02ms  // Maps color values to a point in 3D space
+            
+            time_start = TIME_NOW;
             sendXYZRGBPointcloud(pts, color, buffer);   // 86ms vs 9.7ms
             time_end = TIME_NOW;
 
-            //std::cout << "Frame Time: " << timeMilli(time_end - time_start).count() << " ms" << std::endl;
-            //std::cout << "FPS: " << 1000.0 / timeMilli(time_end - time_start).count() << "\n" << std::endl;
+            std::cout << "Frame Time: " << timeMilli(time_end - time_start).count() \
+                << " ms " << "FPS: " << 1000.0 / timeMilli(time_end - time_start).count() \
+                << std::endl;
             duration_sum += timeMilli(time_end - time_start).count();
         }
     }
     
     pipe.stop();
+    
+    if (send_buffer)
+    {
+        close(client_sock);
+        close(sockfd);
+    }
+
+    free(buffer);
 
     // Use last Frame to display frame Info
     rs2::video_frame color = frames.get_color_frame();
     rs2::depth_frame depth = frames.get_depth_frame();
     rs2::points pts = pc.calculate(depth);
 
-    std::cout << "### Video Frames H x W : " << color.get_height() << " x " << color.get_width() << std::endl;
+    std::cout << "\n### Video Frames H x W : " << color.get_height() << " x " << color.get_width() << std::endl;
     std::cout << "### Depth Frames H x W : " << depth.get_height() << " x " << depth.get_width() << std::endl;
     std::cout << "### # Points : " << pts.size() << std::endl;
     
     std::cout << "\n### Total Frames = " << i << std::endl;
     std::cout << "### AVG Frame Time: " << duration_sum / i << " ms" << std::endl;
-    std::cout << "### AVG FPS: " << 1000.0 / (duration_sum / i) << "\n" << std::endl;
+    std::cout << "### AVG FPS: " << 1000.0 / (duration_sum / i) << std::endl;
+    
+    if (num_of_threads){
+        std::cout << "### OpenMP Threads : " << num_of_threads   << std::endl;
+    }else{
+        std::cout << "### Running Serialized" << std::endl;
+    }
+
     return 0;
 }
 
 
-// Returns the RGB value of a point in the pointcloud.
-std::tuple<uint8_t, uint8_t, uint8_t> get_texcolor(rs2::video_frame texture, rs2::texture_coordinate texcoords) {
-    const int w = texture.get_width(), h = texture.get_height();
-    int x = std::min(std::max(int(texcoords.u*w + .5f), 0), w - 1);
-    int y = std::min(std::max(int(texcoords.v*h + .5f), 0), h - 1);
-    int idx = x * texture.get_bytes_per_pixel() + y * texture.get_stride_in_bytes();
-    const auto texture_data = reinterpret_cast<const uint8_t*>(texture.get_data());
-
-    return std::tuple<uint8_t, uint8_t, uint8_t>(texture_data[idx], texture_data[idx + 1], texture_data[idx + 2]);
-}
 
 // Converts the XYZ values into shorts for less memory overhead,
 // and puts the XYZRGB values of each point into the buffer.
 int copyPointCloudXYZRGBToBuffer(rs2::points& pts, const rs2::video_frame& color, short * pc_buffer) {
 
-    auto vertices = pts.get_vertices();
-    auto tex_coords = pts.get_texture_coordinates();
-    int size = 0;
+    const auto vertices = pts.get_vertices();
+    const rs2::texture_coordinate* tex_coords = pts.get_texture_coordinates();
+    const uint8_t* color_data = reinterpret_cast<const uint8_t*>(color.get_data());
 
-    for (size_t i = 0; i < pts.size() && (5 * size + 2) < BUF_SIZE; i++) {
+    const int pts_size = pts.size();
+    const int w = color.get_width();
+    const int h = color.get_height();
+    const int cl_bp = color.get_bytes_per_pixel();
+    const int cl_sb = color.get_stride_in_bytes();
+    const int w_min = w - 1;
+    const int h_min = h - 1;
+    
+    
+
+// 921 600 Points
+
+#define USE_SIMD
+
+#ifdef USE_SIMD
+
+    const float conv_rate = CONV_RATE;
+    const float point5f = .5f;
+
+    const __m128 _conv_rate = _mm_broadcast_ss(&conv_rate);
+    const __m128 _whwh      = _mm_set_ps(float(w), float(h), float(w), float(h)); // load v
+    const __m128 _f5        = _mm_broadcast_ss(&point5f);
+    const __m128i _zero     = _mm_set_epi32(0, 0, 0, 0);
+    const __m128i _whwh_min = _mm_set_epi32(w_min, h_min, w_min, h_min);
+
+    #pragma omp parallel for schedule(static, 10000) num_threads(num_of_threads)
+    for (int i = 0; i < pts_size; i+=2) {
+
+        int i1 = i;
+        int i2 = i+1;
+
+        auto v1 = vertices[i1];
+        auto v2 = vertices[i2];
+        //float v_temp[4];
+        int v_temp1[4];
+        int v_temp2[4];
+        //short v_temp2[4];
+        int xy1xy2[4];
         
-        // Set cutoff range for pixel points, to lower data size, and omit outlying points
-        //if ((vertices[i].x != 0) && (vertices[i].x < 2) && (vertices[i].x > -2) && (vertices[i].z != 0) && (vertices[i].z < .5)) {
-            
-            //std::tuple<uint8_t, uint8_t, uint8_t> current_color = get_texcolor(color, tex_coords[i]);
+        __m128 _v1 = _mm_set_ps(v1.x, v1.y, v1.z,0); // load v
+        __m128 _v2 = _mm_set_ps(v2.x, v2.y, v2.z,0); // load v
 
-            pc_buffer[size * 5 + 0] = static_cast<short>(vertices[i].x * CONV_RATE);
-            pc_buffer[size * 5 + 1] = static_cast<short>(vertices[i].y * CONV_RATE);
-            pc_buffer[size * 5 + 2] = static_cast<short>(vertices[i].z * CONV_RATE);
-            //pc_buffer[size * 5 + 3] = (short)std::get<0>(current_color) + (short)(std::get<1>(current_color) << 8);
-            //pc_buffer[size * 5 + 4] = std::get<2>(current_color);
+        _v1 = _mm_mul_ps(_v1, _conv_rate); // multiply with _conv_rate
+        _v2 = _mm_mul_ps(_v2, _conv_rate); // multiply with _conv_rate
+        
+        __m128i __v1 = _mm_cvttps_epi32(_v1); // floats to ints
+        __m128i __v2 = _mm_cvttps_epi32(_v2); // floats to ints
+        
+        _mm_store_si128((__m128i*)v_temp1, __v1); // store ints
+        _mm_store_si128((__m128i*)v_temp2, __v2);
 
-            size++;
-        //}
+
+        //Set cutoff range for pixel points, to lower data size, and omit outlying points
+        if (!v1.z) continue;
+        if (!v1.x) continue;
+        if (v1.z > 4) continue;
+        if (!(-2 < v1.x < 2)) continue;
+
+        if (!v2.z) continue;
+        if (!v2.x) continue;
+        if (v2.z > 4) continue;
+        if (!(-2 < v2.x < 2)) continue;
+
+
+        // SIMD
+        __m128 _xy1xy2 = _mm_set_ps(tex_coords[i1].u, tex_coords[i1].v, 
+                                    tex_coords[i2].u, tex_coords[i2].u); // load v
+        _xy1xy2 = _mm_fmadd_ps(_xy1xy2, _whwh, _f5);    // fma
+        __m128i _xy1xy2i = _mm_cvttps_epi32(_xy1xy2);    // floats to ints
+        _xy1xy2i = _mm_max_epi32(_xy1xy2i, _zero);
+        _xy1xy2i = _mm_min_epi32(_xy1xy2i, _whwh_min);
+        _mm_store_si128((__m128i*)xy1xy2, _xy1xy2i);
+        int idx1 = xy1xy2[3] * cl_bp + xy1xy2[2] * cl_sb;
+        int idx2 = xy1xy2[1] * cl_bp + xy1xy2[0] * cl_sb;
+
+        // vs
+
+        // NON SMID       
+        // int x1 = std::min(std::max(int(tex_coords[i1].u*w + .5f), 0), w_min);
+        // int y1 = std::min(std::max(int(tex_coords[i1].v*h + .5f), 0), h_min);
+        // int x2 = std::min(std::max(int(tex_coords[i2].u*w + .5f), 0), w_min);
+        // int y2 = std::min(std::max(int(tex_coords[i2].v*h + .5f), 0), h_min);
+        // int idx1 = x1 * cl_bp + y1 * cl_sb;
+        // int idx2 = x2 * cl_bp + y2 * cl_sb;
+
+        
+        pc_buffer[i * 5 + 0] = v_temp1[3];
+        pc_buffer[i * 5 + 1] = v_temp1[2];
+        pc_buffer[i * 5 + 2] = v_temp1[1];
+
+        // pc_buffer[i * 5 + 0] = static_cast<short>(v.x * CONV_RATE);
+        // pc_buffer[i * 5 + 1] = static_cast<short>(v.y * CONV_RATE);
+        // pc_buffer[i * 5 + 2] = static_cast<short>(v.z * CONV_RATE);
+
+        pc_buffer[i * 5 + 3] = color_data[idx1] + (color_data[idx1 + 1] << 8);
+        pc_buffer[i * 5 + 4] = color_data[idx1 + 2];
+
+
+        pc_buffer[i * 5 + 5] = v_temp2[3];
+        pc_buffer[i * 5 + 6] = v_temp2[2];
+        pc_buffer[i * 5 + 7] = v_temp2[1];
+
+        pc_buffer[i * 5 + 8] = color_data[idx2] + (color_data[idx2 + 1] << 8);
+        pc_buffer[i * 5 + 9] = color_data[idx2 + 2];
+
+
+        //__m256d pd_a = _mm256_set_pd(0, tf_mat[8], tf_mat[4], tf_mat[0]);
+
+    }
+    
+    return pts_size;
+
+#else
+
+    // TODO Optimize return size
+
+    //for (int i = 0; i < pts.size() && (5 * size + 2) < BUF_SIZE; i++) {
+
+    // use 4 threads
+    #pragma omp parallel for schedule(static, 10000) num_threads(num_of_threads)
+    for (int i = 0; i < pts_size; i++) {
+
+        //Set cutoff range for pixel points, to lower data size, and omit outlying points
+        // if (!vertices[i].z) continue;
+        // if (!vertices[i].x) continue;
+        // if (vertices[i].z > 4) continue;
+        // if (!(-2 < vertices[i].x < 2)) continue;
+                
+        int x = std::min(std::max(int(tex_coords[i].u*w + .5f), 0), w_min);
+        int y = std::min(std::max(int(tex_coords[i].v*h + .5f), 0), h_min);
+        
+        int idx = x * cl_bp + y * cl_sb;
+        
+        pc_buffer[i * 5    ] = static_cast<short>(vertices[i].x * CONV_RATE);
+        pc_buffer[i * 5 + 1] = static_cast<short>(vertices[i].y * CONV_RATE);
+        pc_buffer[i * 5 + 2] = static_cast<short>(vertices[i].z * CONV_RATE);
+        pc_buffer[i * 5 + 3] = color_data[idx] + (color_data[idx + 1] << 8);
+        pc_buffer[i * 5 + 4] = color_data[idx + 2];
     }
 
-    return size;
+    return pts_size;
+
+#endif
 }
 
 void sendXYZRGBPointcloud(rs2::points pts, rs2::video_frame color, short * buffer) {
@@ -185,5 +382,5 @@ void sendXYZRGBPointcloud(rs2::points pts, rs2::video_frame color, short * buffe
     size = 5 * size * sizeof(short);
     memcpy(buffer, &size, sizeof(int));
 
-    //send(client_sock, (char *)buffer, size + sizeof(int), 0);
+    if (send_buffer) send(client_sock, (char *)buffer, size + sizeof(int), 0);
 }
